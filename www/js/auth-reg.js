238a639f743fb7353ec461fa8c9572a3819d87c2
Vanessa-176 <nyandav.v.k@gmail.com>
2026-01-21 14:35:30 +0200

    updated authentication ui

---
+++ www/js/auth-reg.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	document.querySelector('.signup form').addEventListener('submit', function(event) {
+	event.preventDefault();
+	// Handle sign up form submission
+	});
+
+	document.querySelector('.login form').addEventListener('submit', function(event) {
+		event.preventDefault();
+		// Handle login form submission
+	});
+});
+
